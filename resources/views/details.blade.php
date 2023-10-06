a72e8e8417757f899247f07bc1eb4e075be66c1b
Penh Seyha <penhseyha4980@gmail.com>
2023-10-06 12:21:40 +0700

    done

---
+++ resources/views/details.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.layout')
+@section('content')
+    <div class="flex-center position-ref full-hright">
+        <div class="content">
+            <div class="title m-b-md">
+                Pizza List - {{ $id }}
+            </div>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PizzaController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/pizza', [PizzaController::class, 'index']);
+Route::get('/pizza/{id}', [PizzaController::class, 'show']);
